6cf0c4aabccdaf9deb7acabd7cc7418048a31e36
Maarma <Maarma25@gmail.com>
2024-04-10 13:59:02 +0300

    update

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -7,7 +7,9 @@ class GameBoard {
         this.boardSize = boardSize;
     }
 
-    draw ( snakeCoordinates ) {
+    draw ( snakeCoordinates, food ) {
+
+        this.gameBoardTable.innerHTML = '';
 
         for ( let i=0; i < this.boardSize; i++) {
 
@@ -21,6 +23,11 @@ class GameBoard {
 
                 if ( snakeCoordinates.includes(id) ){
                     cellTd.classList.add('snake');
+                }
+
+                const foodCoordinates = food.y +'-' + food.x;
+                if ( id == foodCoordinates ){
+                    cellTd.classList.add('food');
                 }
 
                 rowTr.append(cellTd);
